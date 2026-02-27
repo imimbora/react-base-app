7b9621cff33e3dac5dcc06025224d60928757710
DESKTOP-GRM1V3Q\KBDS <imimbora@gmail.com>
2026-02-27 15:37:49 +0900

    storybook setting

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,26 +1,25 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
-export default defineConfig([
-  js.configs.recommended,
-  tseslint.configs.recommended,
-  {
-    files: ["**/*.{js,ts,jsx,tsx}"],
-    plugins: {
-      react: pluginReact,
-    },
-    rules: {
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    languageOptions: {
-      globals: globals.browser,
+export default defineConfig([js.configs.recommended, tseslint.configs.recommended, {
+  files: ["**/*.{js,ts,jsx,tsx}"],
+  plugins: {
+    react: pluginReact,
+  },
+  rules: {
+  },
+  settings: {
+    react: {
+      version: "detect",
     },
   },
-]);
+  languageOptions: {
+    globals: globals.browser,
+  },
+}, ...storybook.configs["flat/recommended"]]);
